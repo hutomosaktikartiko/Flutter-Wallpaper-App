5712105011e8aef452b4f2973b05628831dff065
unknown <hutomosaktikartiko.siskom@gmail.com>
2020-07-16 23:08:08 +0700

    Changing Page on Tap of the Bottom Navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wallyapp/pages/account_page.dart';
+import 'package:wallyapp/pages/explore_page.dart';
+import 'package:wallyapp/pages/favorite_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,12 +10,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedPageIndex = 0;
+
+  var _pages = [ExplorePage(), FavoritePage(), AccountPage()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
+      body: _pages[_selectedPageIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
