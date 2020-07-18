32ef153450ee40a844d29186ffdfe28ecdbb1049
unknown <hutomosaktikartiko.siskom@gmail.com>
2020-07-18 21:55:21 +0700

    Load Images in Account Page

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,7 +1,12 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:wallyapp/config/config.dart';
 import 'package:wallyapp/pages/add_wallpaper_page.dart';
+import 'package:wallyapp/pages/wallpaper_detail_page.dart';
 
 class AccountPage extends StatefulWidget {
   @override
@@ -12,14 +17,7 @@ class _AccountPageState extends State<AccountPage> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   FirebaseUser _user;
 
-  var image = [
-    "https://images.unsplash.com/photo-1540071870804-6264aa727cf7?ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
-    "https://images.unsplash.com/photo-1594905103927-de6aacc5c9d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "https://images.unsplash.com/photo-1542641197-cdc5b5d1923f?ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80",
-    "https://images.unsplash.com/photo-1593106578502-27fa8479d060?ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80",
-    "https://images.unsplash.com/photo-1594878323962-561fbd6357d9?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-    "https://images.unsplash.com/photo-1594878323863-3781779efca6?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  ];
+  final Firestore _db = Firestore.instance;
 
   @override
   void initState() {
@@ -84,21 +82,62 @@ class _AccountPageState extends State<AccountPage> {
                       ],
                     ),
                   ),
-                  StaggeredGridView.countBuilder(
-                    crossAxisCount: 2,
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
-                    itemCount: image.length,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    itemBuilder: (ctx, index) {
-                      return ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image(image: NetworkImage(image[index])));
-                    },
-                  ),
+                  StreamBuilder(
+                      stream: _db
+                          .collection("wallpapers")
+                          .where("uploaded_by", isEqualTo: _user.uid)
+                          .orderBy("date", descending: true)
+                          .snapshots(),
+                      builder:
+                          (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasData) {
+                          return StaggeredGridView.countBuilder(
+                            crossAxisCount: 2,
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            staggeredTileBuilder: (int index) =>
+                                StaggeredTile.fit(1),
+                            itemCount: snapshot.data.documents.length,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20,
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            itemBuilder: (ctx, index) {
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              WallpaperDetailPage(
+                                                image: snapshot
+                                                    .data
+                                                    .documents[index]
+                                                    .data['url'],
+                                              )));
+                                },
+                                child: Hero(
+                                  tag: snapshot
+                                      .data.documents[index].data['url'],
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    // child: Image(image: NetworkImage(image[index]))
+                                    child: CachedNetworkImage(
+                                        placeholder: (ctx, url) => Image(
+                                            image: AssetImage(
+                                                "assets/placeholder.jpg")),
+                                        imageUrl: snapshot
+                                            .data.documents[index].data['url']),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }
+                        return SpinKitChasingDots(
+                          color: primaryColor,
+                          size: 50,
+                        );
+                      }),
                 ],
               )
             : LinearProgressIndicator(),
